0d4da9dbae0addd2f207bf7e1bc3639e3c98b224
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-14 00:32:30 +0300

    complete day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,28 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+//        1 способ - конкатенация через обычный класс String
+
+        long startTimeOne = System.currentTimeMillis();
+        StringBuilder lineOne = new StringBuilder("0");
+        for (int i = 1; i <= 20000; i++) {
+            lineOne.append(" ");
+            lineOne.append(i);
+        }
+        long stopTimeOne = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTimeOne - startTimeOne));
+        System.out.println(lineOne);
+
+//        2 способ - конкатенация через класс StringBuilder
+
+        long startTimeTwo = System.currentTimeMillis();
+        StringBuilder lineTwo = new StringBuilder("0");
+        for (int i = 1; i <= 20000; i++) {
+            lineTwo.append(" ");
+            lineTwo.append(i);
+        }
+        long stopTimeTwo = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTimeTwo - startTimeTwo));
+        System.out.println(lineTwo);
     }
 }
